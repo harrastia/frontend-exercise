84e1f74ada71bab49f0e8f088305fd302a0eb777
Alexandr Lukyanov <alex@sc5.io>
2014-07-02 03:51:52 +0600

    Added app

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,43 @@
+'use strict';
 
+var racingBoard = angular.module('racingBoard', [
+  'ngRoute',
+  'raceControllers'
+]);
+
+/* Router */
+racingBoard.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/:view_name', {
+        templateUrl: 'static/templates/pilots.html',
+        controller: 'PilotsController'
+      }).
+      when('/standings/:team_id', {
+        templateUrl: 'static/templates/standings_board.html',
+        controller: 'StandingsBoardController'
+      }).
+      otherwise({
+        redirectTo: '/pilots'
+
+      });
+  }]);
+
+/* Filters */
+/*
+* Filter the pilots of the team
+* @param teamId {number}  the number/id of the team
+* @return {object} the list of pilots of the team,
+* */
+racingBoard.filter('byTeam', function(){
+  return function(input, teamId){
+    if (typeof input === 'undefined') {
+      return false;
+    }
+    var pilots = function(item) {
+      return item.team == teamId;
+    };
+
+    return input.filter(pilots);
+  };
+});
