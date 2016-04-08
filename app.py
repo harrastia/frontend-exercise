84d5c2709927381b41663030c7bc436efb853ee7
Anssi Kääriäinen <anssi.kaariainen@thl.fi>
2016-04-08 17:02:58 +0300

    Added position to return value

--- app.py
+++ app.py
@@ -8,10 +8,7 @@ from flask import Flask, jsonify
 SRC_DIR = os.path.abspath(os.path.dirname(__file__))
 DATA_DIR = os.path.join(SRC_DIR, 'data')
 
-# I'll load the files in memory for this exercise, usage
-# of real database would of course be a more solid approach,
-# but it is probably overkill for the purposes of fulfilling
-# this exercise.
+# Load the files into global memory - points counters will be reset after reload.
 with codecs.open(os.path.join(DATA_DIR, 'drivers.json'), 'r', 'UTF-8') as drivers_f:
     drivers = json.loads(drivers_f.read())
 
@@ -26,20 +23,40 @@ def home():
     return app.send_static_file('index.html')
 
 
+def add_position(drivers):
+    # If a driver has tied points with the previous one,
+    # the position needs to be the same as the previous
+    # driver's.
+    prev_points = None
+    prev_pos = None
+    drivers = sorted(drivers, key=lambda x: x['points'], reverse=True)
+    for pos, driver in enumerate(drivers):
+        if prev_points != driver['points']:
+            driver['position'] = prev_pos = pos + 1
+            prev_points = driver['points']
+        else:
+            driver['position'] = prev_pos
+
+
+def add_team_name(drivers):
+    # For exercises sake, lets do a in-Python join between drivers
+    # and teams. Nested loop is efficient enough for small amount
+    # of items.
+    for driver in drivers:
+        for team in teams:
+            if team['id'] == driver['team']:
+                driver['team_name'] = team['team']
+
+
 @app.route('/api/standings.json')
 def standings():
     idx = random.randint(0, len(drivers) - 1)
     drivers[idx]['points'] += 1
-    # For exercises sake, lets do a in-Python join between drivers
-    # and teams. Nested loop is efficient enough for small amount
-    # of items.
     # Need to do a copy, as otherwise we would be altering the
     # global data (which we do want for the increment of points).
     drivers_copy = copy.deepcopy(drivers)
-    for driver in drivers_copy:
-        for team in teams:
-            if team['id'] == driver['team']:
-                driver['team_name'] = team['team']
+    add_position(drivers_copy)
+    add_team_name(drivers_copy)
     return json.dumps(drivers_copy)
 
 
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # -*- encoding: UTF-8 -*-
-from app import app, drivers
+from app import app, drivers, add_position
 import unittest
 import json
 
@@ -10,7 +10,7 @@ class FlaskrTestCase(unittest.TestCase):
         app.config['TESTING'] = True
         self.app = app.test_client()
         # Make sure tests don't interfere with each other by resetting
-        # the points counter.
+        # the points counters.
         for item in drivers:
             item['points'] = 0
 
@@ -27,9 +27,24 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_standings_team_data(self):
         response = json.loads(self.app.get('api/standings.json').data)
-        nico_data = response[2]
+        nico_data = [d for d in response if d['team'] == 2 and d['country'] == 'de'][0]
         self.assertEqual(nico_data['driver'], 'Nico Rosberg')
         self.assertEqual(nico_data['team_name'], 'Mercedes AMG Petronas F1 Team')
+
+    def test_standings_position(self):
+        mock_data = [
+            {'key': '1', 'points': 2},
+            {'key': '2', 'points': 1},
+            {'key': '3', 'points': 2}]
+
+        def get_position(key):
+            for item in mock_data:
+                if item['key'] == key:
+                    return item['position']
+        add_position(mock_data)
+        self.assertEqual(get_position('1'), 1)
+        self.assertEqual(get_position('3'), 1)
+        self.assertEqual(get_position('2'), 3)
 
     def test_team_data(self):
         response = self.app.get('api/team/1.json')
